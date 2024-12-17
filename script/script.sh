493b4d9bc328012cc6519339af2d2f0a665af2f4
Lucia Heredia <luciaheredia22@gmail.com>
2024-12-17 13:24:10 +0200

    step 1 done

---
+++ script/script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+##### Variables #####
+# path to directory of backup script
+source_dir="/mnt/c/Users/lucia/PycharmProjects/AutomatedBackupBashScript/script" # change to your own
+# path to directory of backup file
+destination_dir="/mnt/c/Users/lucia/PycharmProjects/AutomatedBackupBashScript/backup" # change to your own
+# name given to backup file folder at the end
+backup_file_name="backup_$date.tar.gz"
+
+# 2. Create a Group
+
+
+# 3. Configure Group Permissions
+
+
+# 4. Create a User
+
+
+# 5. Schedule the Backup Script with Cron
+
+
+# 6. Testing
+
+
+# 7. Automation and Monitoring
+
+
+
