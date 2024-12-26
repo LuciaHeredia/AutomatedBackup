cb9ca017403269c9fd43a1be34d08b1de4072e5a
Lucia Heredia <luciaheredia22@gmail.com>
2024-12-26 22:45:57 +0200

    script done

--- script.sh
+++ script.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-##### Getting Variables Data
-read -p "Enter path of folder to backup: " source_dir
-read -p "Enter path of destination bakup: " destination_dir
-backup_file_name="backup_$(date '+%Y-%m-%d_%H:%M:%S')"
-
+##### Setting up Directories
+source_dir="/source/directory/path" # change
+destination_dir="/destination/directory/path" # change
 # checking path are for directories
 if [ ! -d $source_dir ] || [ ! -d $destination_dir ]; then
-  echo "ERROR: Source and destination should be directories."
+  echo "ERROR Backing Up: Source and destination should be directories."
   exit 1
 fi
 
 ##### Compress source directory with tar:
+backup_file_name="backup_$(date '+%Y-%m-%d_%H:%M:%S')"
 echo "Backing up $source_dir to $destination_dir/$backup_file_name"
 tar -czf "$destination_dir/$backup_file_name.tar.gz" "$source_dir"
 # check backup success
 if [ ! -f "$destination_dir/$backup_file_name.tar.gz" ]; then
-	echo "Error backing up."
+	echo "ERROR Backing Up."
 else
-	echo "Back up finished!"
+	echo "Back Up Finished!"
 	ls -lh $destination_dir # list of files in destination backup
 fi
 
